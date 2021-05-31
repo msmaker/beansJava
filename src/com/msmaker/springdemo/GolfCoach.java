6c446a8e5d235f8bc29b9d4729525515387023ce
Marco Silva <marcosilva.tec@gamil.com>
2021-05-31 13:03:51 -0300

    Inversion of Control

---
+++ src/com/msmaker/springdemo/GolfCoach.java
@@ -0,0 +1,10 @@
+package com.msmaker.springdemo;
+
+public class GolfCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your putting skills for 10 hours today";
+	}
+
+}
